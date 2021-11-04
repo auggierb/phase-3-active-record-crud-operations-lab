29f23a29d7c32beafe327c67684719de7eded23b
August <auggierutter@gmail.com>
2021-11-04 16:50:01 -0400

    commit tree c0a2d6ed7b82aa0087048e7b05f7298734188229

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,25 @@
 class Movie < ActiveRecord::Base
 
+ def self.create_with_title(title)
+   titled_movie = Movie.create(title: title)
+ end 
+ def self.first_movie()
+    Movie.find(1)
+ end
+ def self.last_movie()
+    Movie.find(5)
+end
+def self.movie_count()
+   movie= Movie.all
+   movie.length
+end
+def self.find_movie_with_id(id)
+    Movie.find(id)
+end 
+def self.find_movie_with_attributes(attr)
+    Movie.find_by(attr)
+end
+def self.find_movies_after_2002()
+    Movie.where(release_date:2002)
+end
 end
